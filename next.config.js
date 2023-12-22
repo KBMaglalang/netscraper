af68edc342d13f835baac4bce86daa87dded2440
KBMaglalang <kbm126@live.com>
2023-12-22 12:56:53 -0800

    fix: add nextjs watchoptions for refresh on windows development

--- next.config.js
+++ next.config.js
@@ -12,6 +12,20 @@ const nextConfig = {
       },
     ],
   },
+
+  webpack: (config, _) => {
+    // If there's an existing watchOptions configuration, spread it here; otherwise, just an empty object
+    const watchOptions = config.watchOptions || {};
+
+    return {
+      ...config,
+      watchOptions: {
+        ...watchOptions,
+        poll: 800, // Check for changes every 800ms
+        aggregateTimeout: 300, // Delay the rebuild after the first change for 300ms
+      },
+    };
+  },
 };
 
 module.exports = nextConfig;
