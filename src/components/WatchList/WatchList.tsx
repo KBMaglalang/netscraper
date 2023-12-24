57cd5e0b08ca118eaf04ea6b39507e0333937091
Kurt Maglalang <kbm126@live.com>
2023-12-24 11:12:58 -0800

    feat: move revalidate to page

--- src/components/WatchList/WatchList.tsx
+++ src/components/WatchList/WatchList.tsx
@@ -8,8 +8,6 @@ import { ProductCard } from '../Common/ProductCard';
 // constants and functions
 import { getAllProducts } from '@/lib/actions';
 
-export const revalidate = 60;
-
 export const WatchList = async () => {
   const allProducts = await getAllProducts();
 
